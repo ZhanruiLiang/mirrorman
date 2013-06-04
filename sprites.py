c7da284185b7c51352573ee74a774da6657b7779
apir8181 <kazenoyumechen@gmail.com>
2013-06-04 23:46:25 +0800

    add camera supportand fix mirrors bug

--- sprites.py
+++ sprites.py
@@ -281,7 +281,8 @@ class Emitter(Item):
         light.end = item
 
 class Light:
-    color = glcolor(0, 0x91, 0xe5, 0x88)
+    #color = glcolor(0, 0x91, 0xe5, 0x88)
+    color = glcolor(0xff, 0, 0, 0xff)
     def __init__(self):
         self.nodes = []
 
